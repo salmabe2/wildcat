0bdc50019122c3471fbf7fa136465f05a0a3d4ec
salmabe2 <salmabe231@gmail.com>
2024-01-16 11:20:07 +0100

    feat: Add layout components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'home', component: HomeComponent }, { path: '', component: HomeComponent }];
