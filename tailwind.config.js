b92fcae1b50025f5663a45e90ce3d82501a8690e
salmabe2 <salmabe231@gmail.com>
2024-02-14 02:53:44 +0100

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary_color)',
+          1: 'var(--primary_color_tint_1)',
+          2: 'var(--primary_color_tint_2)',
+        },
+        light: 'var(--light_color)',
+        dark: 'var(--dark_color)'
+      }
+    },
   },
   plugins: [],
 }
