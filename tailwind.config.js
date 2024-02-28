d968855b0335524abb8d70ad6489d91651d717b5
salmabe2 <salmabe231@gmail.com>
2024-02-28 09:44:00 +0100

    style: Add some basic styles in app.component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,12 @@ module.exports = {
         primary: {
           DEFAULT: 'var(--primary_color)',
           1: 'var(--primary_color_tint_1)',
-          2: 'var(--primary_color_tint_2)',
+          2: 'var(--primary_color_tint_2)'
         },
         light: 'var(--light_color)',
         dark: 'var(--dark_color)'
       }
-    },
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
