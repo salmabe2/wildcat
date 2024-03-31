f8233d4106e1c9f9c6865c4ee9219e86ee8ce667
salmabe2 <salmabe231@gmail.com>
2024-03-31 08:29:15 +0200

    feat(overview-study): add new properties

--- src/app/features/study-areas/overview-study-areas/overview-study-areas.component.ts
+++ src/app/features/study-areas/overview-study-areas/overview-study-areas.component.ts
@@ -1,33 +1,30 @@
 import { Component } from '@angular/core';
-import { TableModule } from 'primeng/table';
-import { TableComponent } from "../../../shared/components/table/table.component";
+
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { StudyArea } from '@wildcat/interfaces';
+import { TableComponent} from "@wildcat/shared/components"
+import { TableColumn } from '@wildcat/shared/interfaces';
+
 @Component({
-    selector: 'app-overview-study-areas',
-    standalone: true,
-    templateUrl: './overview-study-areas.component.html',
-    styleUrl: './overview-study-areas.component.scss',
-    imports: [TableModule, TableComponent, CommonModule, RouterModule]
+  selector: 'app-overview-study-areas',
+  standalone: true,
+  templateUrl: './overview-study-areas.component.html',
+  styleUrl: './overview-study-areas.component.scss',
+  imports: [TableComponent, CommonModule, RouterModule]
 })
 export class OverviewStudyAreasComponent {
+  studyAreas: StudyArea[] = [
+    { id: 1, name: 'Madrid', presence: 10, absence: 20, region: 'Comunidad de Madrid', coords: '23 45' }
+  ];
 
-  studyAreas = [
-    { id: 1, name: 'Madrid', cats_num: '10'},
-    { id: 2,  name: 'Guadarrama', cats_num: '10'},
-    { id: 11, name: 'Illescas', cats_num: '10'},
-    { id: 213, name: 'Sierra del Cura', cats_num: '10'},
-    { id: 211, name: 'Madrid', cats_num: '10'},
-    { id: 3, name: 'Madrid', cats_num: '10'},
-    { id: 41, name: 'Madrid', cats_num: '10'},
-    { id: 15, name: 'Madrid', cats_num: '10'},
-  ]
-
-  tableColumns = [
-    { field: 'name', label: 'Nombre'},
-    { field: 'num_cats', label: 'Núm. gatos'},
-    { field: 'detail', label: ''}
-  ]
-
+  tableColumns: TableColumn[] = [
+    { field: 'name', label: 'Nombre' },
+    { field: 'presence', label: 'Presencia' },
+    { field: 'absence', label: 'Ausencia' },
+    { field: 'region', label: 'Comunidad Autónoma' },
+    { field: 'coords', label: 'Coordenadas' },
+    { field: 'link', label: '' }
+  ];
 }
