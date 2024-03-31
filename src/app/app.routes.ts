35beab7c118317fbf51cea2f9dba9386e4839751
salmabe2 <salmabe231@gmail.com>
2024-03-31 07:48:14 +0200

    feat(app.routes): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,13 @@
-import { HomeComponent } from './pages/home/home.component';
+import { StudyAreaItemComponent } from './features/study-areas/study-area-item/study-area-item.component';
+import { HomeComponent } from './features/home/home.component';
 import { Routes } from '@angular/router';
+import { PublicationsComponent } from './features/publications/publications/publications.component';
+import { OverviewStudyAreasComponent } from './features/study-areas/overview-study-areas/overview-study-areas.component';
 
-export const routes: Routes = [{ path: 'home', component: HomeComponent }, { path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'publications', component: PublicationsComponent },
+  { path: 'study-areas', component: OverviewStudyAreasComponent },
+  { path: 'study-areas/:id', component: StudyAreaItemComponent },
+];
