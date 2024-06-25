e3ec30467df4495e1b203588d471d4e5222ed92a
salmabe2 <salmabe231@gmail.com>
2024-06-25 19:05:25 +0200

    feat(): update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,22 @@ import { HomeComponent } from './features/home/home.component';
 import { Routes } from '@angular/router';
 import { PublicationsComponent } from './features/publications/publications/publications.component';
 import { OverviewStudyAreasComponent } from './features/study-areas/overview-study-areas/overview-study-areas.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   { path: 'home', component: HomeComponent },
-  { path: '', component: HomeComponent },
-  { path: 'publications', component: PublicationsComponent },
-  { path: 'study-areas', component: OverviewStudyAreasComponent },
-  { path: 'study-areas/:id', component: StudyAreaItemComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'publications', component: PublicationsComponent },
+      { path: 'study-areas', component: OverviewStudyAreasComponent },
+      { path: 'study-areas/:id', component: StudyAreaItemComponent }
+    ]
+  }
 ];
