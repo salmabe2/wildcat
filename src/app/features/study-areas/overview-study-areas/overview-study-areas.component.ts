a2a09d937024d81b69a60d2a29870191e21c3237
salmabe2 <salmabe231@gmail.com>
2024-11-04 18:56:55 +0100

    feat(): Add study area service

--- src/app/features/study-areas/overview-study-areas/overview-study-areas.component.ts
+++ src/app/features/study-areas/overview-study-areas/overview-study-areas.component.ts
@@ -1,31 +1,35 @@
-import { Component } from '@angular/core';
+import { StudyAreaService } from './../../../core/services/study-area.service';
+import { Component, OnInit } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { StudyArea } from '@wildcat/interfaces';
-import { TableComponent} from "@wildcat/shared/components"
+import { TableComponent } from '@wildcat/shared/components';
 import { TableColumn } from '@wildcat/shared/interfaces';
 
 @Component({
-  selector: 'app-overview-study-areas',
-  standalone: true,
-  templateUrl: './overview-study-areas.component.html',
-  styleUrl: './overview-study-areas.component.scss',
-  imports: [TableComponent, CommonModule, RouterModule]
+	selector: 'app-overview-study-areas',
+	standalone: true,
+	templateUrl: './overview-study-areas.component.html',
+	styleUrl: './overview-study-areas.component.scss',
+	imports: [TableComponent, CommonModule, RouterModule]
 })
-export class OverviewStudyAreasComponent {
-  // TODO: Get data from external file
-  studyAreas: StudyArea[] = [
-    { id: 1, name: 'Madrid', presence: 10, absence: 20, region: 'Comunidad de Madrid', coords: '23 45' }
-  ];
+export class OverviewStudyAreasComponent implements OnInit {
+	constructor(private studyAreaService: StudyAreaService) {}
 
-  tableColumns: TableColumn[] = [
-    { field: 'name', label: 'Nombre' },
-    { field: 'presence', label: 'Presencia' },
-    { field: 'absence', label: 'Ausencia' },
-    { field: 'region', label: 'Comunidad Autónoma' },
-    { field: 'coords', label: 'Coordenadas' },
-    { field: 'link', label: '' }
-  ];
+	studyAreas!: StudyArea[];
+
+	tableColumns: TableColumn[] = [
+		{ field: 'name', label: 'Nombre' },
+		{ field: 'presence', label: 'Presencia' },
+		{ field: 'absence', label: 'Ausencia' },
+		{ field: 'region', label: 'Comunidad Autónoma' },
+		{ field: 'coords', label: 'Coordenadas' },
+		{ field: 'link', label: '' }
+	];
+
+	ngOnInit(): void {
+		this.studyAreas = this.studyAreaService.studyAreas;
+	}
 }
