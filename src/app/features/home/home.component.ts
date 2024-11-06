a73cd8ff961c2397c011f8aa22e8ed9fd5d20202
salmabe2 <salmabe231@gmail.com>
2024-11-06 20:26:52 +0100

    chore(home.component.ts): remove unused code

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -15,56 +15,6 @@ import { TagModule } from 'primeng/tag';
 	styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-	responsiveOptions: any[] = [
-		{
-			breakpoint: '1024px',
-			numVisible: 5
-		},
-		{
-			breakpoint: '768px',
-			numVisible: 3
-		},
-		{
-			breakpoint: '560px',
-			numVisible: 1
-		}
-	];
-
-	images: Object[] = [
-		{
-			itemImageSrc:
-				'https://primefaces.org/cdn/primeng/images/galleria/galleria1.jpg',
-			thumbnailImageSrc:
-				'https://primefaces.org/cdn/primeng/images/galleria/galleria1s.jpg',
-			alt: 'Description for Image 1',
-			title: 'Title 1'
-		},
-		{
-			itemImageSrc:
-				'https://primefaces.org/cdn/primeng/images/galleria/galleria2.jpg',
-			thumbnailImageSrc:
-				'https://primefaces.org/cdn/primeng/images/galleria/galleria2s.jpg',
-			alt: 'Description for Image 2',
-			title: 'Title 2'
-		},
-		{
-			itemImageSrc:
-				'https://primefaces.org/cdn/primeng/images/galleria/galleria3.jpg',
-			thumbnailImageSrc:
-				'https://primefaces.org/cdn/primeng/images/galleria/galleria3s.jpg',
-			alt: 'Description for Image 3',
-			title: 'Title 3'
-		},
-		{
-			itemImageSrc:
-				'https://primefaces.org/cdn/primeng/images/galleria/galleria4.jpg',
-			thumbnailImageSrc:
-				'https://primefaces.org/cdn/primeng/images/galleria/galleria4s.jpg',
-			alt: 'Description for Image 4',
-			title: 'Title 4'
-		}
-	];
-
 	showIndicatorsOnItem: boolean = false;
 	recentPosts!: Publication[];
 
