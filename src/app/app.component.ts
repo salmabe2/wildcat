c325fa7231f17331afc8f3ec7127ee1293d3125c
salmadev2 <salmadev231@gmail.com>
2025-01-27 23:05:38 +0100

    chore: remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './layout/header/header.component';
-
 @Component({
     selector: 'app-root',
     imports: [CommonModule, RouterOutlet],
