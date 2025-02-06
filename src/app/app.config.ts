97c7fb2f1635f4d69d207679911d489b5b143560
salmadev2 <salmadev231@gmail.com>
2025-02-06 15:57:15 +0100

    fix: refresh, enable hash routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -11,7 +11,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideRouter(routes),
+		provideRouter(routes, withHashLocation()),
 		provideHttpClient(),
 		provideAnimationsAsync(),
 		providePrimeNG({
