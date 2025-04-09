64e70a57ff8a89e34ab7a33872207b166eb4485d
salmadev2 <salmadev231@gmail.com>
2025-04-09 17:22:04 +0200

    fix: styles, add primeng layer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,7 +19,11 @@ export const appConfig: ApplicationConfig = {
 				preset: customPreset,
 				options: {
 					prefix: 'p',
-					darkModeSelector: '',
+          darkModeSelector: '',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng'
+          }
 				}
 			}
     }),
