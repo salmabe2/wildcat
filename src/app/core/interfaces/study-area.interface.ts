fd4430518a8b08c9b5a6e209948e595322536051
salmadev2 <salmadev231@gmail.com>
2025-06-21 10:00:40 +0200

    feat: add new data

--- src/app/core/interfaces/study-area.interface.ts
+++ src/app/core/interfaces/study-area.interface.ts
@@ -1,8 +1,15 @@
 export interface StudyArea {
-    id: number
-    name: string,
-    presence: boolean,
-    num_cats: number,
-    region: string,
-    link: string
+	contact?: string;
+	link?: string;
+	months?: string;
+	partners?: string[];
+	presence: boolean;
+	province: string;
+	region: string;
+	year?: string;
+	leader: string;
+	name: string;
+	otherName: string;
+	leaderEmail: string;
+	photos?: string;
 }
--- src/app/features/study-areas/overview-study-areas/overview-study-areas.component.ts
+++ src/app/features/study-areas/overview-study-areas/overview-study-areas.component.ts
@@ -6,25 +6,27 @@ import { RouterModule } from '@angular/router';
 import { StudyAreaService } from '@wildcat/services';
 import { StudyArea } from '@wildcat/interfaces';
 import { TableColumn } from '@wildcat/shared/interfaces';
-import { ButtonComponent } from "@wildcat/shared/components";
+import { ButtonComponent } from '@wildcat/shared/components';
 import { TableComponent } from '@wildcat/shared/components';
 
 @Component({
 	templateUrl: './overview-study-areas.component.html',
 	styleUrl: './overview-study-areas.component.css',
-	imports: [TableComponent, CommonModule, RouterModule, ButtonComponent]
+	imports: [TableComponent, CommonModule, RouterModule, ButtonComponent],
 })
 export default class OverviewStudyAreasComponent implements OnInit {
-  private studyAreaService = inject(StudyAreaService);
+	private studyAreaService = inject(StudyAreaService);
 
 	public studyAreas = signal<StudyArea[]>([]);
 
 	public tableColumns = signal<TableColumn[]>([
 		{ field: 'name', label: 'Nombre', sort: true },
 		{ field: 'presence', label: 'Presencia', sort: true },
-		{ field: 'num_cats', label: 'Núm. gatos', sort: true },
+		{ field: 'province', label: 'Provincia', sort: true },
 		{ field: 'region', label: 'Comunidad Autónoma', sort: true },
-		{ field: 'link', label: '' }
+		{ field: 'year', label: 'Año', sort: true },
+		{ field: 'partners', label: 'Colaboradores', sort: true },
+		{ field: 'leaderEmail', label: 'Contacto', sort: true },
 	]);
 
 	ngOnInit(): void {
